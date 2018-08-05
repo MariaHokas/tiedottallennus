8c6521e3d61e8829f19ae4059bb229e113d91d93
Ahokas Maria Katariina <maria.ahokas@stud.pointcollege.fi>
2018-08-05 17:27:29 +0300

    Totetutettu lämpötilan asettaminen ja tallennus tiedostoon

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+
+            int lämpötila = 0;
+            Console.Write("Anna uusi lämpätila: ");
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+
+            // tiedostoon kirjoittaminen
+            string tiedosto = @"C:\Users\maria\OneDrive\Asiakirjat\harjoitus\Lämpötila.txt";
+
+            // C-kielit katsovat kenoviivan erikoismerkiksi, joten toimiakseen kenomerkki tulee laittaa tuplana tai alkuun @-merkki
+
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu ");
+            Console.ReadLine();
         }
     }
 }
